de365715591668c906c8fd0dfe1c0d05bb62a749
VishalChamkura <vishalchamkuraa@gmail.com>
2025-03-04 14:44:04 +0000

    add static and forms

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
 
 # Create your views here.
 def review(request):
     if request.method == "POST":
-        entered_username = request.POST['username']
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you/")
-    return render(request,"reviews/review.html")
+        form = ReviewForm(request.POST)
+        # entered_username = request.POST['username']
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect("thank-you")
+    else:    
+        form = ReviewForm()    
+    return render(request,"reviews/review.html",{
+        "form":form
+    })
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
