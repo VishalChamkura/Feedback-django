f38f9fdd33478451d3249bb2644dee6aab74953d
VishalChamkura <vishalchamkuraa@gmail.com>
2025-03-10 14:51:39 +0000

    database

--- reviews/views.py
+++ reviews/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from .models import Review
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+@csrf_protect
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         # entered_username = request.POST['username']
 
         if form.is_valid():
-            print(form.cleaned_data)
-            return redirect("thank-you")
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating'])
+            review.save()    
+            return HttpResponseRedirect("/thank-you")
     else:    
         form = ReviewForm()    
     return render(request,"reviews/review.html",{
