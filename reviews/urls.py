d14f9ecd7ef9f5950edfc73761afa7fea14fe915
VishalChamkura <vishalchamkuraa@gmail.com>
2025-03-29 12:41:43 +0000

    add session

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.ReviewView.as_view()),
     path("thank-you",views.ThankyouView.as_view(),name="review_name"),
     path("reviews",views.ReviewsListView.as_view()),
+    path("reviews/favorite",views.AddFavoriteView.as_view()),
     path("reviews/<int:pk>",views.SingleReviewView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -107,3 +107,19 @@ class SingleReviewView(DetailView):
     #     selected_review = Review.objects.get(pk=review_id)
     #     context["review"] = selected_review
     #     return context
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
+
+class AddFavoriteView(View):
+    def post(self,request):
+        review_id = request.POST["review_id"]    
+        # fav_review = Review.objects.get(pk=review_id)         
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
